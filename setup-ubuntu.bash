384ba277949be1209a7063f23ac17fae32683b7d
946336 <wesleywei@protonmail.com>
2019-05-29 16:08:30 -0700

    Unbreak vim install a little more

--- setup-ubuntu.bash
+++ setup-ubuntu.bash
@@ -50,7 +50,7 @@ setup_vim_maybe_ycm() {
         install_target=vim
     fi
 
-    ./install-dotfiles.bash "$install_target"
+    sudo ./install-dotfiles.bash "$install_target"
 
     popd
     popd
